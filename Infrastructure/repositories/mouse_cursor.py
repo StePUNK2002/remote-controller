8b9df091600f44a2e303f21039c81ccd00c1c7bc
StePUNK2002 <edavkin14@gmail.com>
2026-02-13 21:54:24 +0400

    попытка поправить работу скролла

--- Infrastructure/repositories/mouse_cursor.py
+++ Infrastructure/repositories/mouse_cursor.py
@@ -50,7 +50,7 @@ class MouseCursorRepositoryImpl(IMouseCursorRepository):
     
     def scroll_up(self) -> bool:
         try:
-            pyautogui.scroll(10)
+            pyautogui.scroll(50)
             return True
         except Exception as e:
             print(f"Ошибка {e}")
@@ -58,7 +58,7 @@ class MouseCursorRepositoryImpl(IMouseCursorRepository):
 
     def scroll_down(self) -> bool:
         try:
-            pyautogui.scroll(-10)
+            pyautogui.scroll(-50)
             return True
         except Exception as e:
             print(f"Ошибка {e}")
